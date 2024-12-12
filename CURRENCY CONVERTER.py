df8886ac1dc16f5f823b6107aa7abbef4c60ddbe
Sumit627507 <sumit.raut24@pccoepune.org>
2024-12-12 00:11:37 -0800

    Add files via upload

---
+++ CURRENCY CONVERTER.py
@@ -0,0 +1,18 @@
+.def currency_converter():
+    
+
+    print("Currency Converter")
+    print("Supported currencies: USD, EUR, INR, GBP, AUD, etc.")
+
+    from_currency = input("Enter the base currency (e.g., USD): ").upper()
+    to_currency = input("Enter the target currency (e.g., INR): ").upper()
+    amount = float(input("Enter the amount to convert: "))
+
+    try:
+        converted_amount = c.convert(from_currency, to_currency, amount)
+        print(f"{amount} {from_currency} is equal to {converted_amount:.2f} {to_currency}")
+    except Exception as e:
+        print("Error:", e)
+
+if __name__ == "__main__":
+ 
